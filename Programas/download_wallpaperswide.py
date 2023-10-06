012dc8f691f9795b5a72ceae72cbcfbf6ef982bd
emmanuel156 <emmanuel156@gmail.com>
2023-10-05 19:04:53 -0600

    Mejorado el manejo de parametros

--- Programas/download_wallpaperswide.py
+++ Programas/download_wallpaperswide.py
@@ -1,58 +1,128 @@
-import subprocess
-from urllib.request import urlopen
-import os
-from random import choice
+import requests
 from bs4 import BeautifulSoup
+from os.path  import basename
+import os
+import time 
+import sqlite3
+import argparse
+import configparser
 
-website = "http://wallpaperswide.com"
-directory = "C:\\Users\\emman\\OneDrive\\Imágenes\\Papel tapiz\\"
-resolution = "1920x1080"
-url = urlopen(website + "/" + resolution + "-wallpapers-r.html")
+resoluciones = []
 
-def get_pagination(url):
-	code = url.read()
-	soup = BeautifulSoup(code, "lxml")
-	num_listas = []
-	for numbers in soup.find_all('div', {'class': 'pagination'}):
-		num_listas.append(numbers.find_all('a'))
-	tag = num_listas[0][-2]
-	highest_number = tag.next_element
-	pagination_list = [x for x in range(int(highest_number) + 1)]
-	get_all_images(choice(pagination_list))
+def cargar_argumentos_desde_archivo(args):
+	config = configparser.ConfigParser()
+	config.read('download_wallpaperswide.ini')
+	if not args.reintentos and 'Configuracion' in config:
+		args.reintentos = config['Configuracion'].get('reintentos', None)
+	if not args.tiempoespera and 'Configuracion' in config:
+		args.tiempoespera = config['Configuracion'].get('tiempoespera', None)
+	if not args.profundidad and 'Configuracion' in config:
+		args.profundidad = config['Configuracion'].get('profundidad', None)
+	if not args.profundidad and 'Configuracion' in config:
+		args.profundidad = config['Configuracion'].get('paginas', None)
+	carga_resoluciones = config.items("Resoluciones")
+	for key, resolucon in carga_resoluciones:
+		resoluciones.append(resolucon)
+	return args
 
-def get_all_images(pagination):
-	if pagination == 0 or pagination == 1:
-		url = urlopen(website + "/" + resolution + "-wallpapers-r.html")
+def configurar_parser():
+	parser = argparse.ArgumentParser(description='')
+	parser.add_argument('busqueda', nargs='?', default='') 
+	parser.add_argument('--reintentos', help='Reintentos de las descarga')
+	parser.add_argument('--tiempoespera', help='Tiempo de espera entre reintentos')
+	parser.add_argument('--profundidad', help='Profundidad en la descarga de de relacionados')
+	parser.add_argument('--paginas', help='Paginas a descargar (Por default todas)')
+	parser.add_argument('--redescarga', default=False, action='store_true', help='')
+	return parser
+
+def cortar_cadena(texto):
+	indice_interrogacion = texto.find('?')
+	if indice_interrogacion >= 0:
+		return texto[:indice_interrogacion]
 	else:
-		url = urlopen(website + "/" + resolution + "-wallpapers-r/page/" + str(pagination))
-	code = url.read()
-	soup = BeautifulSoup(code, "lxml")
-	all_wall = soup.find_all("li", {'class': 'wall'})
-	soup = BeautifulSoup(str(all_wall), "lxml")
-	links = []
-	for link in soup.findAll('a'):
-		img_url = link.get('href')
-		if img_url not in links:
-			links.append(img_url)
-			img_full_url = website + img_url
-			download_image(img_full_url)
+		return texto
 
-def download_image(img_full_url):
-	url = urlopen(img_full_url)
-	html_code = url.read()
-	soup = BeautifulSoup(html_code, "lxml")
-	for link in soup.find_all(id="wallpaper-resolutions"):
-		for downloadable_link in link.find_all('a'):
-			if resolution in downloadable_link.get('href'):
-				full_image_url = urlopen(website + downloadable_link.get('href'))
-				filename = downloadable_link.get('href').replace('/download/','')
-				print (filename)
-				output = full_image_url.read()
-				image_out = open(directory + filename, "wb")
-				image_out.write(output)
-				image_out.close()
-				abs_path = os.path.abspath(image_out.name)
-				break
+def verificar_archivo_descargado(db_cursor, hash_archivo):
+	db_cursor.execute("SELECT COUNT(*) FROM archivos_descargados WHERE hash = ?", (hash_archivo,))
+	resultado = db_cursor.fetchone()
+	return resultado[0] > 0
+
+def guardar_archivo_descargado(db_cursor, hash_archivo):
+	db_cursor.execute("INSERT INTO archivos_descargados (hash) VALUES (?)", (hash_archivo,))
+	db_cursor.connection.commit()
+
+def get_with_retries(url):
+	intentos_realizados = 0
+	print ('Solicitando: ' + url)
+	while intentos_realizados < int(args.reintentos):
+		try:
+			response = requests.get(url, timeout= int(args.tiempoespera))
+			if response.status_code == 200:
+				return response
+			else:
+				pass
+		except requests.RequestException as e:
+			pass
+		intentos_realizados += 1
+		time.sleep(int(args.tiempoespera))
+	raise Exception(f"No se pudo completar la solicitud.")
+
+def downloadmanga(l, profundidadactual=0, paginaactual=0):
+	profundidadactual = profundidadactual + 1
+	try:
+		r = get_with_retries(l)
+		soup = BeautifulSoup(r.text, 'lxml')
+	except:
+		return
+
+	imgs = soup.findAll('a', attrs = {'target' : '_self'})
+	for img in imgs:
+		for res in resoluciones:
+			#print (img.text)
+			if img.text == res:
+				if args.redescarga or not verificar_archivo_descargado(cursor, cortar_cadena(img.get('href'))):
+					if os.path.exists(res):
+						pass
+					else:
+						os.makedirs(res)
+					try:
+						with open(os.path.join(res , basename(cortar_cadena(img.get('href')))),"wb") as f:
+							f.write(get_with_retries('https://wallpaperswide.com' + cortar_cadena(img.get('href'))).content)
+						guardar_archivo_descargado(cursor, cortar_cadena(img.get('href')))
+					except:
+						pass
+
+	divs = soup.findAll('div', attrs = {'class' : 'mini-hud', 'id':'hudtitle'})
+	for div in divs:
+		pages = div.findChildren()
+		for page in pages:
+			if 'href' in page.attrs:
+				if len(page.get('href')) > 0:
+					if profundidadactual > int(args.profundidad):
+						return
+					downloadmanga('https://wallpaperswide.com' + page.get('href'), profundidadactual, paginaactual)
+
+	divs = soup.findAll('div', attrs = {'class' : 'pagination'})
+	for div in divs:
+		pages = div.findAll('a')
+		for page in pages:
+			if page.text == 'Next »':
+				if len(page.get('href')) > 0:
+					paginaactual = paginaactual + 1
+					if paginaactual < int(args.paginas) or int(args.paginas) == 0:
+						downloadmanga('https://wallpaperswide.com' + page.get('href').replace(' ','%20'), 0, paginaactual)
+
+conexion = sqlite3.connect('download_wallpaperswide.db')
+cursor = conexion.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS archivos_descargados (hash TEXT PRIMARY KEY)")
+
+parser = configurar_parser()
+args = parser.parse_args()
+args = cargar_argumentos_desde_archivo(args)
 
 if __name__ == '__main__':
-	get_pagination(url)
+	search = str(args.busqueda).replace(' ','%20')
+	if len(search) > 0:
+		downloadmanga('https://wallpaperswide.com/search.html?q=' + search)
+	else:
+		downloadmanga('https://wallpaperswide.com/')
